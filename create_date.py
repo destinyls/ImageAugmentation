51a64697bf5101886b4b9def6acda896068fa028
destinyls <1491078277@qq.com>
2021-08-25 14:39:40 +0800

    BCPV2.0

--- create_date.py
+++ create_date.py
@@ -6,10 +6,35 @@ import copy
 import pathlib
 import pickle
 
-import fire
 import numpy as np
-
 from tqdm import tqdm
+
+
+def encode_bbox(P, ry, dims, locs, img_shape):
+    l, h, w = dims[0], dims[1], dims[2]
+    x, y, z = locs[0], locs[1], locs[2]
+    x_corners = [0, l, l, l, l, 0, 0, 0]
+    y_corners = [0, 0, h, h, 0, 0, h, h]
+    z_corners = [0, 0, 0, w, w, w, w, 0]
+    x_corners += - np.float32(l) / 2
+    y_corners += - np.float32(h)
+    z_corners += - np.float32(w) / 2
+    corners_3d = np.array([x_corners, y_corners, z_corners])
+    rot_mat = np.array([[np.cos(ry), 0, np.sin(ry)],
+                        [0, 1, 0],
+                        [-np.sin(ry), 0, np.cos(ry)]])
+    corners_3d = np.matmul(rot_mat, corners_3d)
+    corners_3d += np.array([x, y, z]).reshape([3, 1])
+    corners_3d_extend = corners_3d.transpose(1, 0)
+    corners_3d_extend = np.concatenate(
+        [corners_3d_extend, np.ones((corners_3d_extend.shape[0], 1), dtype=np.float32)], axis=1)    
+    corners_2d = np.matmul(P, corners_3d_extend.transpose(1, 0))
+    corners_2d = corners_2d[:2] / corners_2d[2]
+    bbox = np.array([min(corners_2d[0]), min(corners_2d[1]),
+                     max(corners_2d[0]), max(corners_2d[1])])
+    bbox[[0,2]] = np.clip(bbox[[0,2]], 0, img_shape[1])
+    bbox[[1,3]] = np.clip(bbox[[1,3]], 0, img_shape[0])
+    return bbox
 
 def _read_imageset_file(path):
     with open(path, 'r') as f:
@@ -120,32 +145,46 @@ def create_groundtruth_database(kitti_root,
         num_obj = np.sum(annos["index"] >= 0)
 
         img_path = info["img_path"]
-        img_path = os.path.join(root_path, img_path)
-        img = cv2.imread(img_path)
-        img_shape = img.shape
-        img_shape_key = f"{img.shape[0]}_{img.shape[1]}"
+        img_path_l = os.path.join(root_path, img_path)
+        img_path_r = img_path_l.replace("image_2", "image_3")
+
+        img_l = cv2.imread(img_path_l)
+        img_r = cv2.imread(img_path_r)
+        if np.all(img_l == img_r):
+            print("shape not consistence ...")        
+        img_shape = img_l.shape
+        img_shape_key = f"{img_l.shape[0]}_{img_l.shape[1]}"
+
         for i in range(num_obj):
             if difficulty[i] == -1:
                 continue
             if img_shape_key not in all_db_infos[names[i]].keys():
                 all_db_infos[names[i]][img_shape_key] = []
 
-            filename = f"{image_idx}_{names[i]}_{gt_idxes[i]}_{difficulty[i]}.png"
+            filename = "image_2/" + f"{image_idx}_{names[i]}_{gt_idxes[i]}_{difficulty[i]}.png"
             filepath = database_save_path / filename
             if names[i] in used_classes:
                 if relative_path:
                     relative_filepath = os.path.join(database_save_path.stem, filename)
 
-                box2d = bboxes[i]
-                cropImg = img[int(box2d[1]):int(box2d[3]), int(box2d[0]):int(box2d[2]), :]
-                cv2.imwrite(str(filepath), cropImg)
+                box2d_l = encode_bbox(info['calib/P2'], rotys[i], dims[i], locs[i], img_shape)
+                box2d_r = encode_bbox(info['calib/P3'], rotys[i], dims[i], locs[i], img_shape)
+
+                cropImg_l = img_l[int(box2d_l[1]):int(box2d_l[3]), int(box2d_l[0]):int(box2d_l[2]), :]
+                cropImg_r = img_r[int(box2d_r[1]):int(box2d_r[3]), int(box2d_r[0]):int(box2d_r[2]), :]
+                filepath_l = str(filepath)
+                filepath_r = filepath_l.replace("image_2", "image_3")
+
+                cv2.imwrite(filepath_l, cropImg_l)
+                cv2.imwrite(filepath_r, cropImg_r)
 
                 class_to_label = kitti.get_class_to_label_map()
                 db_info = {
                     "name": names[i],
                     "label": class_to_label[names[i]],
                     "path": relative_filepath,
-                    "bbox": box2d,
+                    "bbox_l": box2d_l,
+                    "bbox_r": box2d_r,
                     "alpha": alphas[i],
                     "roty": rotys[i],
                     "dim": dims[i],
@@ -170,9 +209,9 @@ def create_groundtruth_database(kitti_root,
 
 if __name__ == "__main__":
     kitti_root = "datasets/kitti"
-    # create_kitti_info_file(kitti_root, info_path="kitti_infos") 
-    # create_groundtruth_database(kitti_root, info_path="kitti_infos", database_save_path="gt_database")
+    create_kitti_info_file(kitti_root, info_path="kitti_infos") 
+    create_groundtruth_database(kitti_root, info_path="kitti_infos", database_save_path="gt_database")
 
-    create_kitti_info_file(kitti_root) 
-    create_groundtruth_database(kitti_root)
+    #create_kitti_info_file(kitti_root) 
+    #create_groundtruth_database(kitti_root)
 
